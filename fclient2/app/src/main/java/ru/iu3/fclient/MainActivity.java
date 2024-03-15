1daf3e0c8cb3dd4ec22d2db766a8e46323594d97
tonymal13 <antonmalanin100@gmail.com>
2024-03-15 13:10:58 +0300

    Second commit

--- fclient2/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient2/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,9 +1,20 @@
 package ru.iu3.fclient;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 import java.nio.charset.StandardCharsets;
 
@@ -19,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    ActivityResultLauncher activityResultLauncher;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,20 +40,69 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Example of a call to a native method
+
+
         int num = initRng();
         byte[] key = randomBytes(10);
 
-        String s = "super test text";
+        String s = "Click me";
         byte[] testData = s.getBytes(StandardCharsets.UTF_8);
 
         byte[] encrypted = encrypt(key, testData);
         byte[] decrypted = encrypt(key, encrypted);
 
         String res = new String(decrypted, StandardCharsets.UTF_8);
-        TextView tv = binding.sampleText;
+        TextView tv = binding.sampleButton;
         tv.setText(s);
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                (ActivityResultCallback<ActivityResult>) result-> {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            assert data!=null;
+                            String pin = data.getStringExtra("pin");
+
+                            Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+
+                            synchronized (MainActivity.this){
+                                MainActivity.this.notifyAll();
+                        }
+                    }
+                });
     }
+
+    public void onButtonClick(View v)
+    {
+//        byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
+//        byte[] enc = encrypt(key, stringToHex("000000000000000102"));
+//        byte[] dec = decrypt(key, enc);
+//        String s = new String(Hex.encodeHex(dec)).toUpperCase();
+//        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+
+//        Intent it = new Intent(this, PinpadActivity.class);
+//        startActivity(it);
+
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        activityResultLauncher.launch(it);
+    }
+
+
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
+
 
     /**
      * A native method that is implemented by the 'fclient' native library,
