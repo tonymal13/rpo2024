5bad268387c5a3227be018ab545f699ee323ff08
tonymal13 <antonmalanin100@gmail.com>
2024-04-18 17:27:40 +0300

    almost done

--- front/src/components/Login.jsx
+++ front/src/components/Login.jsx
@@ -1,96 +1,76 @@
-import React from 'react';
-import Home from "./Home";
-import BackendService from "../services/BackendService";
+import React, {useState} from 'react';
+import BackendService from '../services/BackendService';
 import Utils from "../utils/Utils";
-class Login extends React.Component {
+import {useNavigate} from "react-router-dom";
 
+export default function  Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [loggingIn, setLoggingIn] = useState(false);
+    const [submitted, setSubmitted] = useState(false);
+    const [error_message, setErrorMessage] = useState(null);
+    const nav = useNavigate();
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-            loggingIn: false,
-            submitted: false,
-            error_message: null,
-        };
-
-
-
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
+    function handleChangeLogin(e) {
+        setUsername(e.target.value);
     }
 
-    handleChange(e) {
-        const { name, value } = e.target;
-        this.setState({ [name]: value });
+    function handleChangePassword(e) {
+        setPassword(e.target.value);
     }
 
-
-
-    handleSubmit(e) {
+    function handleSubmit(e) {
         e.preventDefault();
-        this.setState({ submitted: true, loggingIn: true, error_message: null });
-        const { username, password } = this.state;
+        setSubmitted(true);
+        setErrorMessage(null);
+        setLoggingIn(true);
         BackendService.login(username, password)
-
             .then ( resp => {
                 console.log(resp.data);
                 Utils.saveUser(resp.data);
+                setLoggingIn(false);
+                nav("/home");
             })
-
-
-
             .catch( err => {
                 if (err.response && err.response.status === 401)
-                    this.setState( { error_message : "Ошибка авторизации", loggingIn: false } );
+                    setErrorMessage("Ошибка авторизации");
                 else
-                    this.setState( { error_message : err.message, loggingIn: false  } );
+                    setErrorMessage(err.message);
+                setLoggingIn(false);
             })
-
-
-            .finally(()=> this.setState({ loggingIn : false }));
     }
 
-
-
-
-
-    render() {
-        //console.log("render");
-        let { submitted, username, password, loggingIn } = this.state;
-        return (
-            <div className="col-md-6 me-0">
-                {this.state.error_message &&
-                    <div className="alert alert-danger mt-1 me-0 ms-0">{this.state.error_message}</div>}
-                <h2>Вход</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="username">Логин</label>
-                        <input type="text" className={'form-control' + (submitted && !username ? ' is-invalid' : '' )}
-                               name="username" value={username}
-                               onChange={this.handleChange} />
-                        {submitted && !username && <div className="help-block text-danger">Введите имя пользователя</div>}
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Пароль</label>
-                        <input type="password" className={'form-control' + (submitted && !password ? ' is-invalid' : '' )}
-                               name="password" value={password}
-                               onChange={this.handleChange} />
-                        {submitted && !password &&
-                            <div className="help-block text-danger">Введите пароль</div>
-                        }
-                    </div>
-                    <div className="form-group mt-2">
-                        <button className="btn btn-primary">
-                            {loggingIn && <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>}
-                            Вход
-                        </button>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+    return  (
+        <div className="col-md-6 me-0">
+            {error_message &&
+                <div className="alert alert-danger mt-1 me-0 ms-0">{error_message}</div>}
+            <h2>Вход</h2>
+            <form name="form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="username">Логин</label>
+                    <input type="text" className={'form-control' + (submitted && !username ? ' is-invalid' : '' )}
+                           name="username" value={username}
+                           onChange={handleChangeLogin} />
+                    {submitted && !username && <div className="help-block text-danger">Введите имя пользователя</div>}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Пароль</label>
+                    <input type="password" className={'form-control' + (submitted && !password ? ' is-invalid' : '' )}
+                           name="password" value={password}
+                           onChange={handleChangePassword} />
+                    {submitted && !password &&
+                        <div className="help-block text-danger">Введите пароль</div>
+                    }
+                </div>
+                <div className="form-group mt-2">
+                    <button className="btn btn-primary">
+                        {loggingIn && <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>}
+                        Вход
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
 }
 
-export default Login;
+
--- front/src/components/NavigationBarClass.jsx
+++ front/src/components/NavigationBarClass.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
+import {faHome, faUser} from '@fortawesome/free-solid-svg-icons'
 import {Link, useNavigate} from 'react-router-dom';
+import Utils from "../utils/Utils";
 
 class NavigationBarClass extends React.Component {
 
@@ -16,6 +17,7 @@ class NavigationBarClass extends React.Component {
     }
 
     render() {
+        let uname = Utils.getUserName();
         return (
             <Navbar bg="light" expand="lg">
                 <Navbar.Brand><FontAwesomeIcon icon={faHome} />{' '}My RPO</Navbar.Brand>
@@ -23,11 +25,16 @@ class NavigationBarClass extends React.Component {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link as={Link} to="/home">Home</Nav.Link>
-
+                        <Nav.Link onClick={this.goHome}>Another Home</Nav.Link>
                         <Nav.Link onClick={() =>{ this.props.navigate("\home")}}>Yet Another Home</Nav.Link>
-
-
                     </Nav>
+                    <Navbar.Text>{uname}</Navbar.Text>
+                    { uname &&
+                        <Nav.Link onClick={this.logout}><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Выход</Nav.Link>
+                    }
+                    { !uname &&
+                        <Nav.Link as={Link} to="/login"><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Вход</Nav.Link>
+                    }
                 </Navbar.Collapse>
             </Navbar>
         );
